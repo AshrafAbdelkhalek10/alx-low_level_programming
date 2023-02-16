586ac0599661ed07a735ce05e2783886d41187ae
AshrafAbdelkhalek10 <abdelkhalekashraf0@gmail.com>
2023-02-16 15:40:26 -0800

    the first task in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - programm to print something by puts
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
+
