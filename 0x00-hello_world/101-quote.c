301cac1e220bff6ed9d949f33b8a796cb81231a6
AshrafAbdelkhalek10 <abdelkhalekashraf0@gmail.com>
2023-02-16 17:50:44 -0800

    the first task in C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<unistd.h>
 /**
  * main - A programm to print one line to stdout error
  * Return: 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
