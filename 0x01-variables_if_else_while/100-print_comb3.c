ff994414a953c16d02e1926e5aa6fe0c2a969e57
AshrafAbdelkhalek10 <abdelkhalekashraf0@gmail.com>
2023-02-17 17:29:28 -0800

    1-1tsk

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry programm
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
