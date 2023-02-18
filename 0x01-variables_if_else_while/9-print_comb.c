15205d8cc35f34e8cf2d933098045c4fa6a50092
AshrafAbdelkhalek10 <abdelkhalekashraf0@gmail.com>
2023-02-17 16:51:04 -0800

    the lst

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry Programm
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		putchar(i + '0');
+		if (i < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('/n');
+	return (0);
+}
