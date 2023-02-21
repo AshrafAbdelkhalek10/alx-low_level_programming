dc680ed9891f165c6df23f5a7cad4425bbba4411
AshrafAbdelkhalek10 <abdelkhalekashraf0@gmail.com>
2023-02-21 13:53:43 -0800

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_last_digit - to print the last digit
+ *@a: The parameter to be checked
+ * Return: 1,0 (Success)
+ */
+int print_last_digit(int a)
+{
+	int rem;
+
+	rem = a % 10;
+	return (rem);
+}
