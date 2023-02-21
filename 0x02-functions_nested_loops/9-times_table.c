2e9549c54c076af5f44470dac8aec8559c960bb7
AshrafAbdelkhalek10 <abdelkhalekashraf0@gmail.com>
2023-02-21 14:33:24 -0800

    time tables

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * times_table - to check if is letter or not
+ * Return: (Success)
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int tmp;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			tmp = 0;
+			tmp = j * i;
+			_putchar(tmp + '0');
+			_putchar(',  ');
+			if (j == 9)
+			{
+				_putchar('$');
+				_putchar('\n');
+			}
+		}
+	}
+}
