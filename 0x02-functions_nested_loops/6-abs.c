26da4cc7991bc04a66c7c427322155952781c259
AshrafAbdelkhalek10 <abdelkhalekashraf0@gmail.com>
2023-02-21 13:43:30 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - to check if is letter or not
+ * @a: The parameter to be checked
+ * Return: 1,0 (Success)
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+		return (a);
+	else
+		return (-a);
+}
