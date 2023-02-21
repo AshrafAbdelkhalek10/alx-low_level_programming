43e901f35fd74e7c46d32b22e73ef9c007c0eb1a
AshrafAbdelkhalek10 <abdelkhalekashraf0@gmail.com>
2023-02-21 15:24:12 -0800

    mandatory

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_to_98 - to print to 98
+ * @n: Parameter
+ * Return : 0 (Success)
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+				printf("%d", n);
+		}
+	}
+	else
+	{
+		for (n; n >= 98 ; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+				printf("%d ", n);
+		}
+	}
+}
