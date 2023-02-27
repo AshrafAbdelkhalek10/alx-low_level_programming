38099e6076cbc1eac6cf968ab394bb725362215a
AshrafAbdelkhalek10 <abdelkhalekashraf0@gmail.com>
2023-02-27 13:17:43 -0800

    random

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,16 +8,14 @@ int main(void) {
   int i;
   char password[PASSWORD_LEN + 1];
 
-  // Seed the random number generator with the current system time
   srand(time(NULL));
 
-  // Generate the random 8-character password
   for (i = 0; i < PASSWORD_LEN; i++) {
     password[i] = 'A' + (rand() % 26);
   }
   password[PASSWORD_LEN] = '\0';
 
-  printf("Generated Password: %s\n", password);
+  printf("%s\n", password);
 
   return 0;
 }
